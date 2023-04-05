9186ad751db18d798fa9c1b8b87f928047f0588c
Matheus Leite <maatheus2710@gmail.com>
2023-04-05 19:58:02 -0300

    MongoDB connection and User Repository and Service implementation

---
+++ src/main/java/com/matheusleite/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.matheusleite.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheusleite.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/matheusleite/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.matheusleite.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheusleite.workshopmongo.domain.User;
+import com.matheusleite.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+}
