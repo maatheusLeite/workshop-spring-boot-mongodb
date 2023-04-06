6e7efd711c9fd7aced5e183a438c8b9299576c68
Matheus Leite <maatheus2710@gmail.com>
2023-04-06 17:45:29 -0300

    Updating User using PUT

--- src/main/java/com/matheusleite/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusleite/workshopmongo/services/UserService.java
@@ -21,9 +21,20 @@ public class UserService {
 		return repository.findAll();
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 	
 	public User insert(User obj) {
