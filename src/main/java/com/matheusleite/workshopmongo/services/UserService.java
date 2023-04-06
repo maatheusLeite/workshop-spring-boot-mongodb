f1c82b76059eb5ba794455625cc04d65a3578685
Matheus Leite <maatheus2710@gmail.com>
2023-04-06 17:24:47 -0300

    Inserting User using POST

--- src/main/java/com/matheusleite/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusleite/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheusleite.workshopmongo.domain.User;
+import com.matheusleite.workshopmongo.dto.UserDTO;
 import com.matheusleite.workshopmongo.exception.ObjectNotFoundException;
 import com.matheusleite.workshopmongo.repository.UserRepository;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
