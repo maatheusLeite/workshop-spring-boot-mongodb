0c826dfdfd1f7324468dda065c819b4ef5991135
Matheus Leite <maatheus2710@gmail.com>
2023-04-07 18:53:33 -0300

    Multi-criteria query

--- src/main/java/com/matheusleite/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/matheusleite/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.matheusleite.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -13,8 +14,11 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 	// '?0' se refere ao primeiro parametro do metodo
 	// 'i' é uma opção do MongoDB para ignorar maiusculas e minusculas
-	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
-	
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	@Query("{ $and: [ { 'date': { $gte: ?1 } }, { 'date': { $lte: ?2 } }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
