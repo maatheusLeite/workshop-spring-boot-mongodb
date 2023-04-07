3d32e9515ce1a4bb3e4caf744d19d30fd89cda3d
Matheus Leite <maatheus2710@gmail.com>
2023-04-07 18:13:01 -0300

    Consulta simples com @Query

--- src/main/java/com/matheusleite/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/matheusleite/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.matheusleite.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.matheusleite.workshopmongo.domain.Post;
@@ -10,5 +11,10 @@ import com.matheusleite.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	// '?0' se refere ao primeiro parametro do metodo
+	// 'i' é uma opção do MongoDB para ignorar maiusculas e minusculas
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") 
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
